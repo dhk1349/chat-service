a16b91ff8a32dc71c69d590d6bf188c9b6f66598
dhk1349 <macuser@MacUserui-MacBookPro-4.local>
2024-06-01 23:21:09 +0900

    setup guide

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # chat-service
+
+
+Chatserver setup
+- curl -fsSL https://ollama.com/install.sh | sh
+
+On terminal1
+- python main.py
+
+
+On terminal2
+- ollama serve 
+
+On terminal3
+- ollama run llama3:8b-instruct-q2_K
--- chatserver/main.py
+++ chatserver/main.py
@@ -3,7 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict
 import uvicorn
+
 import re
+import json
+import requests
 
 regex = r'^https?://localhost(:\d+)?$'
 
@@ -43,26 +46,61 @@ async def chat(message: ChatMessage):
     user_message = message.message
 
     # Load your language model here and generate a response
-    response_text = generate_response(user_message)
-
+    # response_text = generate_response(user_message)
+    
     # Store the chat log
     if session_id not in chat_logs:
-        chat_logs[session_id] = []
+        chat_logs[session_id] = [{"role":"system", "content": "한글만 사용해."}, {"role": "assistant", "content":"안녕하세요. 무엇을 도와드릴까요?"}]
     message_id = len(chat_logs[session_id]) + 1
-    chat_logs[session_id].append({"user": user_message, "bot": response_text})
+    chat_logs[session_id].append({"role": "user", "content": user_message})
+    print(session_id)
+    print(chat_logs[session_id])
+
+    response = llama_chat(chat_logs[session_id])
+    response_text = response["content"]
+    chat_logs[session_id].append({"role": "assistant", "content": response_text})
 
     response = ChatResponse(
         sender="bot",
         text=response_text,
         id=message_id
     )
-
     return response.dict()
+
+def llama_chat(messages):
+    stream = False
+    r = requests.post(
+        "http://0.0.0.0:11434/api/chat",
+        json={"model": "llama3:8b-instruct-q2_K", "messages": messages, "stream": stream},
+	stream=stream
+    )
+    r.raise_for_status()
+    output = ""
+    
+    for line in r.iter_lines():
+        body = json.loads(line)
+        if "error" in body:
+            raise Exception(body["error"])
+        if body.get("done") is False:
+            message = body.get("message", "")
+            content = message.get("content", "")
+            output += content
+            # the response streams one token at a time, print that as we receive it
+            print(content, end="", flush=True)
+
+        if body.get("done", False):
+            if not stream:
+                message = body.get("message", "")
+            else:    
+                message["content"] = output
+            return message
+
 
 # Function to generate a response using your language model
 def generate_response(message):
     # Load your language model and generate a response
     return "This is a sample response."
+
 
 if __name__ == "__main__":
     import os
